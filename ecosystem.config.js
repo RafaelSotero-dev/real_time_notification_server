6e317ff3cb2fc366cb8c8f1dcb0dd80347c6fbec
RafaelSotero-dev <rafaeljesusabc2@gmail.com>
2026-04-03 16:39:08 -0300

    .

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,17 @@ module.exports = {
   apps: [
     {
       name: 'real-time-notification',
-      script: 'npm',
+      script: 'index.js',
+      interpreter: 'node',
       args: 'run start',
       out_file: '/var/log/realtime/out.log',
       error_file: '/var/log/realtime/out.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss',
       time: true,
+      env: {
+        PORT: 3000,
+        FRONTENDURL: process.env.FRONTENDURL || 'http://localhost:5500',
+      },
     },
   ],
 }
